490363d2b58e08e2854bb9ed26a22d034f0b706b
David Chung <dchung@dchung.com>
2023-05-02 09:00:53 -0500

    Updated wait for single node

--- wait.sh
+++ wait.sh
@@ -10,9 +10,7 @@ echo " ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚�
 echo ""
 
 echo -n 'Preparing your environment ...'; 
-timeout 120 bash -c 'until ./node1/bin/cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done';
-timeout 120 bash -c 'until ./node2/bin/cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done';
-timeout 120 bash -c 'until ./node3/bin/cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done';
+timeout 120 bash -c 'until ./node/bin/cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done';
 echo ' DONE!'
 echo "Environment ready."
 echo ""
